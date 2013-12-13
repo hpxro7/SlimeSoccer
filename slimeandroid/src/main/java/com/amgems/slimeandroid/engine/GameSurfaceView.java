d5d7f7eced3654d52d54924931e4ba76314009b3
jteosw <jteosw@gmail.com>
2013-12-13 00:21:48 -0800

    Implemented a little bit of physics

--- slimeandroid/src/main/java/com/amgems/slimeandroid/engine/GameSurfaceView.java
+++ slimeandroid/src/main/java/com/amgems/slimeandroid/engine/GameSurfaceView.java
@@ -18,6 +18,7 @@ public class GameSurfaceView extends SurfaceView implements Renderer {
     private static final int TARGET_WIDTH = 1280;
     private static final int TARGET_HEIGHT = 720;
     private static final int SLIME_SIZE = 150;
+    private static final float GRAVITY = 1800f;
 
     private SurfaceHolder mHolder;
     Bitmap mBackgroundBitmap; // Badass grandma?
@@ -37,15 +38,27 @@ public class GameSurfaceView extends SurfaceView implements Renderer {
         Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
         p.setColor(Color.BLUE);
         canvas.drawRect(0f, 0f, TARGET_WIDTH, 0.9f * TARGET_HEIGHT, p);
-        Log.d("Stuff", "width: " + TARGET_WIDTH + " height: " + TARGET_HEIGHT);
         p.setColor(Color.GRAY);
         canvas.drawRect(0f, 0.9f * TARGET_HEIGHT, TARGET_WIDTH, TARGET_HEIGHT, p);
         mPlayer = new Slime(0.5f * TARGET_WIDTH, 0.9f * TARGET_HEIGHT - 75 , SLIME_SIZE, Color.GREEN);
     }
 
     @Override
-    public void draw(long deltaTime) {
+    public void draw(float deltaTime) {
         Canvas surfaceCanvas = mHolder.lockCanvas();
+        // Get Input/Change Velocity
+        if (mPlayer.y >= 0.9 * TARGET_HEIGHT) mPlayer.dy = 0;
+        else mPlayer.dy += GRAVITY * deltaTime;
+
+        if (mPlayer.dy != 0) Log.d("speed", "speed is " + mPlayer.dy);
+        // Change positions
+        mPlayer.x += deltaTime * mPlayer.dx;
+        mPlayer.y += deltaTime * mPlayer.dy;
+        mPlayer.checkGround();
+        mPlayer.checkBounds();
+
+        // Check for collisions between players/ball
+
         surfaceCanvas.drawBitmap(mBackgroundBitmap, 0, 0, null);
 
         // Draw stuff
