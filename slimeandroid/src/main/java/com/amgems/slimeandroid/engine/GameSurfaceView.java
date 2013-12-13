47c11ae63c517c72ee328f48bb0613f5925820ee
shermpay <shermanpay1991@gmail.com>
2013-12-12 21:37:01 -0800

    Added fixed resolution for GameSurfaceView

--- slimeandroid/src/main/java/com/amgems/slimeandroid/engine/GameSurfaceView.java
+++ slimeandroid/src/main/java/com/amgems/slimeandroid/engine/GameSurfaceView.java
@@ -2,6 +2,8 @@ package com.amgems.slimeandroid.engine;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -11,12 +13,16 @@ import java.util.Random;
  * Created by shermpay on 12/12/13.
  */
 public class GameSurfaceView extends SurfaceView implements Renderer {
+    private static final int TARGET_WIDTH = 1280;
+    private static final int TARGET_HEIGHT = 720;
+
     private SurfaceHolder mHolder;
 
 
     public GameSurfaceView(Context context) {
         super(context);
         mHolder = getHolder();
+        mHolder.setFixedSize(TARGET_WIDTH, TARGET_HEIGHT);
     }
 
     @Override
