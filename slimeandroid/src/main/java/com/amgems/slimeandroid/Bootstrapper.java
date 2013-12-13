005ef464fb926956ecb60588ad55283c0ef30390
Zachary Iqbal <zachary.iq@gmail.com>
2013-12-12 21:10:49 -0800

    Removed actionbar and set default orientation to landscape

--- slimeandroid/src/main/java/com/amgems/slimeandroid/Bootstrapper.java
+++ slimeandroid/src/main/java/com/amgems/slimeandroid/Bootstrapper.java
@@ -17,6 +17,9 @@ public class Bootstrapper extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         mGameSurfaceView = new GameSurfaceView(this);
         mEngine = new GameEngine(mGameSurfaceView);
